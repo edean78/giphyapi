ef74bc2755eff4042be93b260ce8c1dd6c055cea
edean78 <edean78@gmail.com>
2019-10-01 13:26:58 -0400

    Created the ajax call to retrieve data from giphy and display it in the html

--- assets/javascript/giphy.js
+++ assets/javascript/giphy.js
@@ -4,25 +4,25 @@ $(document).ready(function () {
     // Create global varibles
     // Array of selected gifs
     var gifs = [];
+    var apiKey = "wlls0MTP5W9mbTuI6ZqNEJUScOOmKLBR";
+    var queryURL = "http://api.giphy.com/v1/gifs/search?api_key=wlls0MTP5W9mbTuI6ZqNEJUScOOmKLBR&"
 
     // Create the ajax call to call and retrieve data from the api
-    function displayGifInfo() {
+    // function displayGifInfo() {
 
-        var apiKey = "wlls0MTP5W9mbTuI6ZqNEJUScOOmKLBR";
-        var gif = $(this).attr("data-name");
-        var userInput = "";
-        var gueryURL = "https://api.giphy.com/v1/gifs/search?api_key=" + apiKey + "&q=" + userInput;
 
-        $.ajax({
-            url: queryURL,
-            method: "GET"
-        }).then(function (response) {
-            console.log(response);
 
-            // var rating = response.data.0.Rating;
-            // var year = response.data.Year;
-        });
-    }
+
+    //     $.ajax({
+    //         url: url,
+    //         method: "GET"
+    //     }).then(function (response) {
+    //         console.log(response);
+
+    //         // var rating = response.data.0.Rating;
+    //         // var year = response.data.Year;
+    //     });
+    // }
 
     // Create function to load buttons from local storage
     function loadBtns() {
@@ -63,6 +63,52 @@ $(document).ready(function () {
     // Calling the renderButtons function to display the intial buttons
     renderButtons();
 
+    // <div class="giphy-info">
+    //      <div class="card">
+    //          <i class="far fa-star favorite" data-id="${giphy.id}" data-star="false">
+    //           <img src="..." class="card-img-top" alt="...">
+    //           <div class="card-body">
+    //                <h5 class="card-title">Card title</h5>
+    //                <p class="card-text">Some quick example text to build on the card title and make up the bulk of
+    //                         the card's content.</p>
+    //            </div>
+    //      </div>
+
+    // </div>
+
+    //Create function to render the data received from giphy
+    function createGiphy(giphys) {
+        for (var i = 0; i < giphys.length; i++) {
+            var giphy = giphys[i];
+
+            var image = giphy.images;
+
+            var giphyDisplay = `
+            <div class="giphy-info">
+            <div class="card col-md-4">
+                <i class="far fa-star favorite" data-id="${giphy.id}" data-star="false">
+                <img src="${image.original_still.url}"
+                data-still="${image.original_still.url}"
+                data-animate="${image.original.url}"
+                data-state="${giphy.embed_url}" 
+                class="card-img-top" 
+                alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of
+                        the card's content.</p>
+                    </div>
+                    <div class="footer" data-link="${giphy.embed_url}">
+                        <p>Copy Link <i class="fa fa-link"></i></p>
+                    </div>    
+            </div>
+        </div>
+         `;
+
+            $(".giphy").append(giphyDisplay);
+        }
+    }
+
     // This function handles events when a user searches a category
     $("#btn-submit").on("click", function (event) {
         event.preventDefault();
@@ -75,6 +121,18 @@ $(document).ready(function () {
 
         // Calling the renderButtons function to display the intial buttons
         renderButtons();
+
+        var url = queryURL + "q=" + gif;
+
+        $.ajax({
+            url: url,
+            method: "GET"
+        }).then(function (response) {
+            console.log(response)
+            var giphys = response.data;
+
+            createGiphy(giphys);
+        });
     });
 
     // Create a function to delete the dynamically created buttons
@@ -88,11 +146,14 @@ $(document).ready(function () {
         renderButtons();
     };
 
+
+    // displayGifInfo();
+
     // Create event listener when user clicks the X button
     $(document).on("click", ".btn-close", deleteButtons)
 
     // Add a click event listener to all elements wit ha class of ".btn-gifs" 
-    $(document).on("click", ".btn-gifs", displayGifInfo);
+    // $(document).on("click", "#btn-sumbit", displayGifInfo)
 
 
 
@@ -100,3 +161,4 @@ $(document).ready(function () {
 
 
 });
+
